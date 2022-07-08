caee91bd4b332a90446a25fe1fac5a750fe5c481
kaktus <redmadpanda@gmail.com>
2022-07-08 12:48:55 +0500

    add something

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from summator import *
 from minusator import *
 from division import *
-
+from multiplication import *
 # enter(x,y) где x ehjdtym jn 0 до 2, у кол-во задач на уровне
 #Summator().enter(0,3)
 #Minusator().enter(0,2)
-Division().enter(0,3)
+#Division().enter(0,3)
+Multiply().enter(0,3)
--- readme.txt
+++ readme.txt
@@ -2,3 +2,12 @@ It's game for repeating math operations like addition, subtraction, division, mu
 
 For start game type in terminal:
 > python main.py
+
+Game flow: 
+1 Summator
+2 Minusator
+3 Division
+4 Multiplication
+5 Finish
+
+and Dead level if will be incorrect data from user
