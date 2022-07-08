cf97d38a647cc6d76756e186a4957c9c09f93d14
kaktus <redmadpanda@gmail.com>
2022-07-08 12:19:04 +0500

    add some code

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import summator
-import minusator
+from summator import *
+from minusator import *
+from division import *
 
+# enter(x,y) где x ehjdtym jn 0 до 2, у кол-во задач на уровне
 #Summator().enter(0,3)
-Minusator().enter(0,3)
+#Minusator().enter(0,2)
+Division().enter(0,3)
