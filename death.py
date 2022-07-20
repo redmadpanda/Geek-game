eb6c017557f5a8e285e41a10d02fb3eb7fd63c1c
kaktus <redmadpanda@gmail.com>
2022-07-20 11:07:22 +0500

    Change some code, add n, m because in other classes also has n and m

--- death.py
+++ death.py
@@ -8,6 +8,8 @@ class Death():
 		"Что ты наделал?!"
 	]
 
-	def enter(self):
+	# тут вводим n и m потомучто для других функций в других классах это
+	# уровень и количество задач, тут же они не требуются
+	def enter(self, n, m):
 		print(Death.text[randint(0,len(self.text) - 1)])
 		exit(1)
